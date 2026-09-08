21211af0d845fa8f61cad73fc6375708d7ed8d17
Ignacio Bugueño Córdova <ignacio.bugueno@live.com>
2026-09-08 09:08:27 -0300

    Remove shm-size option from Docker run command
    Removed shared memory size limit from Docker run command.

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,6 @@ docker rm -fv ignacio_event_ebird
 docker run -it \
   --gpus '"device=0"' \
   --name ignacio_event_ebird \
-  --shm-size=224g \
   -v /home/ignacio.bugueno/cachefs/event_reconstruction/output/ebird:/app/Rislab_Event_influence_volume \
   -v /home/ignacio.bugueno/cachefs/datasets/processed_data/reconstruction:/app/Rislab_Event_influence_volume/dataset:ro \
   ignacio_event_ebird
