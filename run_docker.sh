f1a9eda592e0e77524269f4180407fee2c997d01
Ignacio Bugueño Córdova <ignacio.bugueno@live.com>
2026-09-08 09:08:00 -0300

    Fix GPU argument syntax in run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 docker rm -fv ignacio_event_ebird
 
 docker run -it \
-  --gpus all \
+  --gpus '"device=0"' \
   --name ignacio_event_ebird \
   --shm-size=224g \
   -v /home/ignacio.bugueno/cachefs/event_reconstruction/output/ebird:/app/Rislab_Event_influence_volume \
